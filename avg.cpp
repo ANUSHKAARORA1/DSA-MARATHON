f63a86fef1d851af419d656cfacac4c76d455b6c
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 10:01:44 +0530

    avg.cpp

---
+++ avg.cpp
@@ -0,0 +1,12 @@
+// average of 3 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+   float a, b, c;
+    cout<<"enter values of a,b,c";
+    cin>>a>>b>>c;
+    float avg;
+    avg=(a+b+c)/3;
+    cout<<"Average : "<<avg;
+}
