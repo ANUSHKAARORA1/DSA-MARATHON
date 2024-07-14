a4cf4e079799dae399d065c9e0d5c1fc452f966c
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 09:53:59 +0530

    Create sumof2num.cpp

---
+++ sumof2num.cpp
@@ -0,0 +1,12 @@
+//sum of 2 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter number";
+    cin>>a>>b;
+    int sum=0;
+    sum=a+b;
+    cout<<"sum is :"<<sum;
+}
