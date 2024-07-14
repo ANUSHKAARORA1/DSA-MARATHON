b5db361b4ae42d51dfca13b92f61f31e89de727b
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 11:44:02 +0530

    pattern8.cpp

---
+++ pattern8.cpp
@@ -0,0 +1,29 @@
+//print pattern 
+/*
+1
+2 3
+3 4 5
+4 5 6 7
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter number";
+cin>>n;
+int i=1;
+while(i<=n)
+{
+    int count=i;
+    int j=1;
+    while(j<=i)
+    {
+        cout<<count<<" ";
+        count++;
+        j++;
+    }
+    cout<<endl;
+    i++;
+}
+}
