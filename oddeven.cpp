da7c6f0fbb75ff22d454c7d0deaa13b358ec9d2f
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 10:03:57 +0530

    oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,13 @@
+// odd and even
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ cout<<"enter number";
+ cin>>n;
+ if(n%2==0)
+ cout<<"even number";
+ else
+ cout<<"odd number";
+}
