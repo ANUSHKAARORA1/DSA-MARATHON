2eed8cb9dabf920008739574974afd81f47ea95f
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 10:38:48 +0530

    even1ton.cpp

---
+++ even1ton.cpp
@@ -0,0 +1,15 @@
+//print even numbers from 1 to n
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter number";
+cin>>n;
+int i=2;
+while(i<=n)
+{
+cout<<i<<" ";
+i=i+2;
+}
+}
