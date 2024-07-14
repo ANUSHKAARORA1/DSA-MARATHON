a921664170e491496355e2561b4db0a8fae0ce07
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 11:35:49 +0530

    pattern5.cpp

---
+++ pattern5.cpp
@@ -0,0 +1,28 @@
+//print pattern 
+/*
+*
+* *
+* * *
+* * * *
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter number";
+cin>>n;
+int i=1;
+int count=1;
+while(i<=n)
+{
+    int j=1;
+    while(j<=i)
+    {
+        cout<<"*"<<" ";
+        j++;
+    }
+    cout<<endl;
+    i++;
+}
+}
