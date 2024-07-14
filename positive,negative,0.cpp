81a2ec531e6ff36a579435307b34c61d43e78f5d
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 10:07:33 +0530

    positive,negative,0.cpp

---
+++ positive,negative,0.cpp
@@ -0,0 +1,15 @@
+// positive , negative or 0
+#include<iostream>
+using namespace std;
+int main()
+{
+ int n;
+ cout<<"enter number";
+cin>>n;
+if(n>0)
+cout<<"positive number";
+else if(n<0)
+cout<<"negative number";
+else
+cout<<"number is 0";
+}
