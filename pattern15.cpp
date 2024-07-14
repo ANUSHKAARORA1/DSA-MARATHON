9dcff2f9aa2d33e879c81f8438a949f16a077798
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-14 20:04:57 +0530

    pattern15.cpp

---
+++ pattern15.cpp
@@ -0,0 +1,29 @@
+//print pattern 
+/*
+A
+B C
+D E F
+G H I J
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    int i=1;
+     char ch='A';
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            cout<<ch<<" ";
+            ch++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
