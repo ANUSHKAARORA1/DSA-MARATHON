6e73836bc58bb70e59a538cde40ff75d7761d975
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-07-19 16:20:06 +0530

    Create km to mile.cpp

---
+++ km to mile.cpp
@@ -0,0 +1,14 @@
+// km to miles
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main() 
+{
+  //  in 1 km total 0.621371 miles
+  float km, miles;
+  cout<<"enter km "<<endl;
+  cin>>km;
+  float totalkm;
+  totalkm=km*0.621371;
+  cout<<totalkm<<"miles";
+}
