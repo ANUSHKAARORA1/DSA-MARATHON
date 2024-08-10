72d4e1812f8dddf5b4ff7353433c3148113659cf
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-08-10 07:43:46 +0530

    Create print reverse in doubly lined list.cpp

---
+++ print reverse in doubly lined list.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    Node *prev;
+    int data;
+    Node *next;
+     Node(int value){
+         data=value;
+         prev=NULL;
+         next=NULL;
+     }
+};
+void insertathead(int value,Node*&head,Node*&tail){
+    //if ll is empty
+    if(head==NULL&&tail==NULL)
+    {
+        Node*newnode=new Node(value);
+        head=newnode;
+        tail=newnode;
+    }
+    else{
+        //ll is not empty
+        Node *newnode=new Node(value);
+        newnode->next=head;
+        head->prev=newnode;
+        head=newnode;
+    }
+}
+void printreverse(Node*tail){
+    Node *temp=tail;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->prev;
+    }
+    cout<<"null";
+}
+void print(Node* head)
+{
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+
+int main()
+{
+Node*head=NULL;
+Node*tail=NULL;
+insertathead(10,head,tail);
+insertathead(20,head,tail);
+insertathead(30,head,tail);
+printreverse(tail);
+    return 0;
+}
